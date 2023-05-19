0991fbeb63c589b80fe6a7bda94a62d5b8d36a83
Ernest <ernest.okiror@student.moringaschool.com>
2023-05-20 02:41:29 +0300

    Completed Assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,57 @@
 def admin_login(username, password)
-  # your code here
+  admin_username = "ADMIN"
+  admin_password = "12345"
+
+  if username.upcase == admin_username && password == admin_password
+    "Access granted"
+  else
+    "Access denied"
+  end
+end
+def hows_the_weather(temperature)
+  weather = case temperature
+            when 0..39
+              "It's brisk out there!"
+            when 40..64
+              "It's a little chilly out there!"
+            when 85..Float::INFINITY
+              "It's too dang hot out there!"
+            else
+              "It's perfect out there!"
+            end
 end
 
-def hows_the_weather(temperature)
-  # your code here
-end
+
+
 
 def fizzbuzz(num)
-  # your code here
+  divisable_by_3 = num % 3==0
+  divisable_by_5 = num % 5==0
+  fizz_buzz = case 
+              when divisable_by_3 && divisable_by_5
+              then   "FizzBuzz"
+              when divisable_by_3 
+                "Fizz"
+              when divisable_by_5 
+                "Buzz"
+              else return num
+              end      
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2.to_f  # Convert num2 to float for accurate division
+  else
+    puts "Invalid operation!"
+    nil
+    
+  end
 end
 
